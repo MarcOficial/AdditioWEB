fba6f86955c6465bcd8cb1ce8ab4435e0e4b0efd
MarcOficial <llabresmarc083@gmail.com>
2025-10-01 17:29:00 +0200

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  plugins: [react()],
-  base: "/additio-clone/", // importante para GitHub Pages
-});
